50f2a6d637942958e00eb11c7f8f3ddd8bfb3ef5
niavisiustinian <iustinian@yahoo.com>
2020-04-14 17:03:54 +0300

    proiectQA19.1

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println( "1." );
+        System.out.println( "Hello" );
+        System.out.println( "Iustinian" );
+        System.out.println( "2." );
+        System.out.println( 7 + 3 );
+        System.out.println( "3." );
+        System.out.println(  (float)  10 / 3 );
+        System.out.println( "4. " );
+        System.out.println( "a.");
+        System.out.println( -5 +   8 * 6   );
+        System.out.println( "b." );
+        System.out.println(( 55 + 9 ) % 9 );
+        System.out.println( "c." );
+        System.out.println( 20 + -3 * 5 / 8 );
+        System.out.println(" d.");
+        System.out.println(  5 + 15 / 3 * 2 - 8 % 3 );
+    }
+
+}
